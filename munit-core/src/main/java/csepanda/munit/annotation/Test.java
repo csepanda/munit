d0680022fa6cbac2a517c4eebf7cd27f60fe0d75
Andrey Bova <andrewbova@yandex.ru>
2019-08-15 15:06:02 +0300

    munit-core: Add javadoc to @Test annotation

--- munit-core/src/main/java/csepanda/munit/annotation/Test.java
+++ munit-core/src/main/java/csepanda/munit/annotation/Test.java
@@ -5,6 +5,26 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+/** The @Test annotation is used to specify methods that will be run by mUnit.
+ *
+ * This annotation can be applied only to public void methods without arguments.
+ *
+ * Example of usage:
+ * <pre>{@code
+ *     public class ExampleTestClass {
+ *          @Test
+ *          public void exampleTest() {
+ *              var expected = 4;
+ *              var actual =  2 + 2;
+ *
+ *              if (expected != actual) {
+ *                  throw new RuntimeException("Expected " + expected + " but got " + actual);
+ *              }
+ *
+ *          }
+ *     }
+ * }</pre>
+ */
 @Target(ElementType.METHOD)
 @Retention(RetentionPolicy.RUNTIME)
 public @interface Test {
