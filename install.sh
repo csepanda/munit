bcc3ed2f1120eee74a5e63ca5ebb6440193e501b
Andrey Bova <andrewbova@yandex.ru>
2019-08-21 15:45:59 +0300

    Fix installation script
    Install from parent package, because installation of single module
    causes non correct installation of package in local repository

--- install.sh
+++ install.sh
@@ -2,11 +2,7 @@
 
 panic() { echo ERROR: "$*"; exit 2; }
 
-mvn package || panic cannot build packages
-
-# install core module
-# `mvn install -pl core` fails to execute due to the some depedencies
-cd munit-core && mvn install && cd ..
+mvn install || panic cannot install packages
 
 # get executable program
 cp munit-launcher/target/munit-launcher-jar-with-dependencies.jar ./munit-launcher.jar
